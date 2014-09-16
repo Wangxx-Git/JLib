e317635fa9a7470d5f6de2f6b84ed656689a9210
decadestory <728246447@qq.com>
2014-09-16 17:32:59 +0800

    log not complete

---
+++ Tools/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+using log4net.Core;
+
+namespace JLib.Tools
+{
+    [assembly: log4net.Config.XmlConfigurator(Watch = true)]
+    public class Logger
+    {
+        public static ILog Log
+        {
+            get { return LogManager.GetLogger("Default"); }
+        }
+    }
+}
